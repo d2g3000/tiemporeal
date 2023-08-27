10360d372b5ba58ef8b7324e37742590b86fbfb6
danny <d2g3000@gmail.com>
2023-08-27 10:47:29 -0400

    importado como modulo

--- netlify/functions/main.js
+++ netlify/functions/main.js
@@ -1,4 +1,3 @@
-import { io } from "https://cdn.socket.io/4.4.1/socket.io.esm.min.js";
 const socket=io();
 const saludar=document.querySelector("#saludar");
 const consulta= document.querySelector("#consulta")
