66528777acecac942da62023b25ac14c1775f145
danny <d2g3000@gmail.com>
2023-08-26 23:47:24 -0400

    catorce

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -14,7 +14,7 @@ const api = express();
 app.use( express.static(path.join(__dirname,"/src/views")))
 const router = Router();
 router.get('/hello',(req,res)=>{
-    res.sendFile(__dirname+"index.html");
+    res.sendFile("index.html");
 });
 io.on("connection",Socket=>{
     Socket.emit("welcome","ahora estas conectado");
