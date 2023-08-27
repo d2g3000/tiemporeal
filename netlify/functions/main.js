95f08e45b69ef3507b5e0e888fb0f9946e48229a
danny <d2g3000@gmail.com>
2023-08-27 11:01:10 -0400

    99

--- netlify/functions/main.js
+++ netlify/functions/main.js
@@ -1,4 +1,4 @@
-import { io } from "socket.io-client";
+const { io } = require("socket.io-client");
 const socket = io();
 const saludar=document.querySelector("#saludar");
 const consulta= document.querySelector("#consulta")
